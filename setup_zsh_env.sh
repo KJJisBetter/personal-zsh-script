b8f0a0b18f881f658f967a0fdb00d3620fa220a8
Kevin Javier <124921826+KJJisBetter@users.noreply.github.com>
2024-08-12 23:12:44 -0400

    Update setup_zsh_env.sh

--- setup_zsh_env.sh
+++ setup_zsh_env.sh
@@ -39,8 +39,8 @@ fi
 # Install Oh My Posh if not already installed
 if ! command -v oh-my-posh &> /dev/null; then
     echo "Installing Oh My Posh..."
-    mkdir ~/.local/bin
-    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+    mkdir ~/.oh-my-posh/bin
+    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.oh-my-posh/bin
 fi
 
 # Create themes directory and download Zen theme for Oh My Posh
